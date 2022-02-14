ab95557584d04f289fd8ca752109cdc698373009
HuuTin <saophaiquay2@gmail.com>
2022-02-14 16:34:17 +0700

    Add authenticaion on routes

--- src/Route/TodoRoute.ts
+++ src/Route/TodoRoute.ts
@@ -4,12 +4,12 @@ import TodoController from "../Controller/TodoController"
 
 const router = Router();
 
-router.get("/",TodoController.GetAllTodos)
-router.get("/detail/:id",TodoController.GetTodoById)
-router.get("/todobyuser/:id",TodoController.GetTodosByUser)
-router.post("/",TodoController.AddTodo)
-router.put("/:id",TodoController.UpdateTodo)
-router.delete("/:id",TodoController.DeleteTodo)
-router.post("/assign/:id",TodoController.AssignTodo)
+router.get("/",Authentication,TodoController.GetAllTodos)
+router.get("/detail/:id",Authentication,TodoController.GetTodoById)
+router.get("/todobyuser/:id",Authentication,TodoController.GetTodosByUser)
+router.post("/",Authentication,TodoController.AddTodo)
+router.put("/:id",Authentication,TodoController.UpdateTodo)
+router.delete("/:id",Authentication,TodoController.DeleteTodo)
+router.post("/assign/:id",Authentication,TodoController.AssignTodo)
 
 export default router
